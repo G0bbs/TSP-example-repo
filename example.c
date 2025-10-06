fecfbff3beeed4948d59846dd5edc340ee3de0e0
Ezra Crocker <74689473+G0bbs@users.noreply.github.com>
2025-10-06 16:18:08 -0400

    Update example.c with user input

--- example.c
+++ example.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char** argv)
 {
   printf("I'm an example c program for TSP");
+  printf("I've been edited in this commit");
+  if (argc > 1) printf("You've supplied an argument %s", argv[1]);
   return 0;
 }
